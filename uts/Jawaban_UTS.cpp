29d68cc9e54370130daf7312b458556d0b075408
root <root@INBook_X1.localdomain>
2021-11-11 18:20:03 +0700

    uts

---
+++ uts/Jawaban_UTS.cpp
@@ -0,0 +1,23 @@
+//20200801177 - Ahmad Andri - UTS - PBO
+//Jawaban nomor 4
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int nomor;
+
+    do{
+        cout<<"MENU\n";
+        cout<<"0. Keluar\n";
+        cout<<"1. Input\n";
+        cin>>nomor;
+
+        switch(nomor){
+            case 0 : cout<<"\t Thanks\n";
+        }
+    }
+
+}
